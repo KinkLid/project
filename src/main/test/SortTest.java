6841e5335ae25939f91b8c1bed5311b17f4f7085
dexipas <sfdxyz@yandex.ru>
2025-11-24 17:33:31 +0300

    Изменение структуры проекта

---
+++ src/main/test/SortTest.java
@@ -0,0 +1,128 @@
+package org.teamwork.test;
+
+import org.teamwork.model.Student;
+import org.teamwork.sort.*;
+
+import java.util.Arrays;
+
+public class SortTest {
+    public static void main(String[] args) {
+        testAverageGradeSort();
+        testGroupNumberSort();
+        testRecordBookNumberSort();
+        testAllSort();
+    }
+    static void testAverageGradeSort(){
+        Student[] result = new Student[]{
+                new Student(105,36.80, "006600"),
+                new Student(105,31.35, "006600"),
+                new Student(105,36.44, "006600"),
+                new Student(105,82.35, "006600"),
+                new Student(105,99.99, "006600"),
+                new Student(105,73.56, "006600"),
+                new Student(105,68.23, "006600"),
+                new Student(105,55.44, "006600"),
+        };
+
+        result = new StudentSort(result, new ByAverageGradeStrategy()).getStudents();
+
+        check(result,new Student[]{
+                new Student(105,31.35, "006600"),
+                new Student(105,36.44, "006600"),
+                new Student(105,36.80, "006600"),
+                new Student(105,55.44, "006600"),
+                new Student(105,68.23, "006600"),
+                new Student(105,73.56, "006600"),
+                new Student(105,82.35, "006600"),
+                new Student(105,99.99, "006600")
+        },"testAverageGrade");
+    }
+
+    static void testGroupNumberSort(){
+        Student[] result = new Student[]{
+                new Student(444,66.00, "006600"),
+                new Student(777,66.00, "006600"),
+                new Student(666,66.00, "006600"),
+                new Student(999,66.00, "006600"),
+                new Student(555,66.00, "006600"),
+                new Student(222,66.00, "006600"),
+                new Student(333,66.00, "006600"),
+                new Student(111,66.00, "006600")
+        };
+
+        result = new StudentSort(result, new ByGroupNumberStrategy()).getStudents();
+
+        check(result,new Student[]{
+                new Student(111,66.00, "006600"),
+                new Student(222,66.00, "006600"),
+                new Student(333,66.00, "006600"),
+                new Student(444,66.00, "006600"),
+                new Student(555,66.00, "006600"),
+                new Student(666,66.00, "006600"),
+                new Student(777,66.00, "006600"),
+                new Student(999,66.00, "006600")
+        },"testGroupNumber");
+    }
+    static void testRecordBookNumberSort(){
+        Student[] result = new Student[]{
+                new Student(105,99.00, "2040289"),
+                new Student(105,99.00, "2040499"),
+                new Student(105,99.00, "2040122"),
+                new Student(105,99.00, "2040447"),
+                new Student(105,99.00, "2040321"),
+                new Student(105,99.00, "1940321"),
+                new Student(105,99.00, "2040111А"),
+                new Student(105,99.00, "2040111Б"),
+        };
+
+        result = new StudentSort(result, new ByRecordBookNumberStrategy()).getStudents();
+
+        check(result,new Student[]{
+                new Student(105,99.00, "1940321"),
+                new Student(105,99.00, "2040111А"),
+                new Student(105,99.00, "2040111Б"),
+                new Student(105,99.00, "2040122"),
+                new Student(105,99.00, "2040289"),
+                new Student(105,99.00, "2040321"),
+                new Student(105,99.00, "2040447"),
+                new Student(105,99.00, "2040499"),
+        },"testRecordBookNumber");
+    }
+
+    static void testAllSort(){
+        Student[] result = new Student[]{
+                new Student(234,66.66, "222222Б"),
+                new Student(345,99.99, "2040479"),
+                new Student(234,66.66, "333333В"),
+                new Student(345,64.74, "2040122"),
+                new Student(123,44.44, "006Б600"),
+                new Student(123,55.55, "00В6600"),
+                new Student(345,64.75, "2040644"),
+                new Student(234,66.66, "111111А"),
+                new Student(123,33.33, "0066А00")
+        };
+
+        StudentSort studentSort = new StudentSort(result, new ByGroupNumberStrategy());
+        studentSort.setStrategy(new ByAverageGradeStrategy());
+        studentSort.setStrategy(new ByRecordBookNumberStrategy());
+        result = studentSort.getStudents();
+
+        check(result,new Student[]{
+                new Student(123,33.33, "0066А00"),
+                new Student(123,44.44, "006Б600"),
+                new Student(123,55.55, "00В6600"),
+                new Student(234,66.66, "111111А"),
+                new Student(234,66.66, "222222Б"),
+                new Student(234,66.66, "333333В"),
+                new Student(345,64.74, "2040122"),
+                new Student(345,64.75, "2040644"),
+                new Student(345,99.99, "2040479")
+        },"testAllSort");
+    }
+
+
+    static void check(Student[] result, Student[] expected, String testName){
+        if(Arrays.equals(result,expected)) System.out.println(testName + " PASSED! :)");
+        else System.out.println(testName+" FAILED... :(");
+    }
+}
